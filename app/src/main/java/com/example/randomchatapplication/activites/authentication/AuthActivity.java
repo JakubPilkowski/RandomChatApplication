4c031b110461de134064c6d5e9b3e52d8e303b80
Pilkowski <ilsihaterhd@gmail.com>
2020-04-10 20:16:30 +0200

    Rozpoczeto widoki logowania

---
+++ app/src/main/java/com/example/randomchatapplication/activites/authentication/AuthActivity.java
@@ -0,0 +1,65 @@
+package com.example.randomchatapplication.activites.authentication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.ViewDataBinding;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.randomchatapplication.R;
+import com.example.randomchatapplication.base.BaseActivity;
+import com.example.randomchatapplication.base.BaseFragment;
+import com.example.randomchatapplication.databinding.ActivityAuthBinding;
+import com.example.randomchatapplication.interfaces.Providers;
+import com.example.randomchatapplication.navigation.Navigator;
+
+public class AuthActivity extends BaseActivity<ActivityAuthBinding, AuthActivityViewModel> implements Providers {
+
+
+    @Override
+    protected void initActivity(ActivityAuthBinding binding) {
+        binding.setViewModel(viewModel);
+        viewModel.setProviders(this);
+        viewModel.init();
+    }
+
+    @Override
+    protected Class<AuthActivityViewModel> getViewModel() {
+        return AuthActivityViewModel.class;
+    }
+
+    @Override
+    public int getIdFragmentContainer() {
+        return 0;
+    }
+
+    @Override
+    public int getLayoutRes() {
+        return R.layout.activity_auth;
+    }
+
+    @Override
+    public Activity getActivity() {
+        return this;
+    }
+
+    @Override
+    public Navigator getNavigator() {
+        return navigator;
+    }
+
+    @Override
+    public ViewDataBinding getBinding() {
+        return binding;
+    }
+
+    @Override
+    public ViewDataBinding getActivityOrFragmentBinding() {
+        return getCurrentFragment().binding;
+    }
+
+    @Override
+    public BaseFragment getFragment() {
+        return getCurrentFragment();
+    }
+}
---
+++ app/src/main/java/com/example/randomchatapplication/activites/authentication/AuthActivityViewModel.java
@@ -0,0 +1,9 @@
+package com.example.randomchatapplication.activites.authentication;
+
+import com.example.randomchatapplication.base.BaseViewModel;
+
+public class AuthActivityViewModel extends BaseViewModel {
+
+    public void init() {
+    }
+}
